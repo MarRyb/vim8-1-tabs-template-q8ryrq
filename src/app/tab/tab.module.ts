4c114075d69b2846fc85cda2c28a57ca828c2b3c
MarGer <marinagernyak@gmail.com>
2020-01-27 18:12:30 +0200

    add component for structure tab

--- src/app/tab/tab.module.ts
+++ src/app/tab/tab.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TabTitleComponent } from './tab-title/tab-title.component';
+import { TabContentComponent } from './tab-content/tab-content.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TabTitleComponent, TabContentComponent, TabsComponent, TabComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    TabTitleComponent,
+    TabContentComponent,
+    TabsComponent,
+    TabComponent
   ]
 })
 export class TabModule { }
